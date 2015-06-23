976f7805fff5ed49896affdceaa6ac044d8a2e6f
nikolaystavrev <n.stavrev@abv.bg>
2015-06-23 23:19:41 +0300

    a

--- api/controllers/ClinicController.js
+++ api/controllers/ClinicController.js
@@ -7,15 +7,15 @@
 
 module.exports = {
 	clinics : function(req,res){
-		// var clinics = [
-		// 	{ address : "Казичене 121", position:[42.696703, 23.319827]},
-		// 	{ address : "Младост 2 Блок 251", position:[42.690142, 23.319827]},
-		// 	{ address : "Лозенец улица Борова гора 6", position : [42.691782, 23.314076] }
-		// ];
-		// Clinic.create(clinics, function(err, clinics){
-		// 	if(err) throw err;
-		// 	console.log(clinics);
-		// })
+		var clinics = [
+			{ address : "Казичене 121", position:[42.696703, 23.319827]},
+			{ address : "Младост 2 Блок 251", position:[42.690142, 23.319827]},
+			{ address : "Лозенец улица Борова гора 6", position : [42.691782, 23.314076] }
+		];
+		Clinic.create(clinics, function(err, clinics){
+			if(err) throw err;
+			console.log(clinics);
+		})
 		return res.view({layout : null});
 	},
 	appointment : function(req,res){
